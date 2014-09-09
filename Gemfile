f633dbf72765225d3cca5fa40b3e99f44a7c9405
Nigel Blackmore <nigel@blackmore.de>
2014-09-10 00:48:56 +0200

    removed old files

--- Gemfile
+++
@@ -1,5 +0,0 @@
-#encoding: UTF-8
-
-source 'http://rubygems.org'
-
-gem 'thin'
--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
+$(function() {
 
+    // # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // No super no frills basic addEventListener the dark theme
+    // # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    var el = document.querySelector("#darkTheme");
+    el.addEventListener('toggle', function (e) {
+      if (e.detail.isActive) {
+        //put all that you want this change in this finction 
+        $('#main-header').addClass('dark');
+        $('.main-body').addClass('dark');
+        $('.img-logo-connect').addClass('dark');
+      } else {
+        $('#main-header').removeClass('dark');
+        $('.main-body').removeClass('dark');
+        $('.img-logo-connect').removeClass('dark');
+      };
+    });
 
-  $(function() {
-      var el = document.querySelector("#darkTheme");
-      el.addEventListener('toggle', function (e) {
-        if (e.detail.isActive) {
-          //console.log(e.target, e.detail.isActive);
-          $('#main-header').addClass('dark');
-          $('.main-body').addClass('dark');
-          $('.img-logo-connect').addClass('dark');
-        } else {
-          $('#main-header').removeClass('dark');
-          $('.main-body').removeClass('dark');
-          $('.img-logo-connect').removeClass('dark');
-        };
-      });
-  
 });
